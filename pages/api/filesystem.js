da46507a3af6abe8d87ebafc9c89f59c6a61752f
thomashutze <84834771+thomashutze@users.noreply.github.com>
2021-09-03 18:23:53 +0200

    a

---
+++ pages/api/filesystem.js
@@ -0,0 +1,17 @@
+// Backend aaa
+import formidable from 'formidable';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.uploadDir = "../../public/uploads";
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    console.log(err, fields, files);
+  });
+};
---
+++ pages/music/thefuckingvirgins.js
@@ -0,0 +1,6 @@
+function thefuckingvirgins() {
+  return <div>droge kurk</div>
+
+}
+
+export default thefuckingvirgins
